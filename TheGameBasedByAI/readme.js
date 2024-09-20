a37536e3cb679e29b3e75223ccca8effb69f6eca
Cai-xiaobai-0 <68491718+Cai-xiaobai-0@users.noreply.github.com>
2024-09-20 15:10:52 +0800

    Update readme.js

--- TheGameBasedByAI/readme.js
+++ TheGameBasedByAI/readme.js
@@ -1,7 +1,11 @@
 /**
- * 基于AI的游戏开发(自然语言游戏开发)
- * 原理:训练AI识别游戏相关概念,基于提供的素材(同理可以使用AI生成素材)以及游戏描述实时渲染游戏画面
- * 例如:AI识别不同方块所代表的含义、不同按键所映射的功能、各种剧情交互等功能实时渲染一个供游玩的游戏世界
- * 备注:
- *      1.可以提供图像
+ * 基于通用人工智能(AI)的游戏开发(自然语言游戏开发)
+ * Game development based on general artificial intelligence.(Natural language game development)
+ * 原理：训练AI识别游戏相关概念,基于提供的素材(同理可以使用AI生成素材)以及游戏描述实时渲染游戏画面
+ * Principle: Train AI to recognize game related concepts. Based on the provided materials(AI can be used to generate materials) and game description. Real time rendering of game graphics.
+ * 例如：沙盒游戏中AI识别不同方块所代表的含义、不同按键所映射的功能等实时渲染一个供游玩的游戏世界
+ * Such as: The sandbox games. AI recognizes the meanings represented by different blocks and renders a real-time game world for gameplay.
+ * Other:
+ *   1.可以提供图像使AI生成的世界与作者所想一致
+ *     Can provide images to make the AI generated world conform to the author's expectations
  */
