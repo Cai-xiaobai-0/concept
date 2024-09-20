2eaa9637871c1b2a6f1349b7f732af2548084ec0
Cai-xiaobai-0 <68491718+Cai-xiaobai-0@users.noreply.github.com>
2024-09-20 14:53:07 +0800

    Update readme.js

--- TheComputerBasedByAI/readme.js
+++ TheComputerBasedByAI/readme.js
@@ -1,5 +1,6 @@
 /**
- * 基于AI的计算机(自然语言处理)(自然语言编程[汉字编程])
- * 原理:计算机本身即是一个强大的AI,通过软件相关定义文件实时处理用户输入的数据
- * 例如:'贾维斯'
+ * 基于通用人工智能(AI)的计算机(自然语言编程[汉字编程])
+ * The Computer Based By AI (Natural Language Programming[Text Programming])
+ * 原理:计算机本身即是一个强大的AI,通过定义文件实时处理用户输入的数据
+ * Principle:The computer itself is a powerful AI. Real time processing of user input data through defined files.
  */
